db9e52a89362113632c6f2b46742be994c701606
mazinmazu <safahsaf444@gmail.com>
2023-02-15 15:32:29 +0500

    Change in wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -21,7 +21,7 @@
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define( 'DB_NAME', 'organic-phase2' );
-
+dfdsfdsfdsfds
 /** Database username */
 define( 'DB_USER', 'root' );
 
